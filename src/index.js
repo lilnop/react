898d8f330b5fbaa7414007baa474e1e4935b05a1
Allen <alleneben@gmail.com>
2019-08-23 09:09:52 -0400

    Displaying quotes every 3 seconds

--- src/index.js
+++ src/index.js
@@ -3,12 +3,36 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 class App extends Component{
-    words = ["one","two","three"];
+    // i declared a constructor
+    constructor(props){
+        super(props)
+
+        this.state = {
+            words: ["one","two","three"],
+            newquote: 'Quote will start changing after 2 seconds'
+        }
+
+        this.displayquotes = this.displayquotes.bind(this);
+    }
+
+    displayquotes(){
+
+        setInterval(() => {
+            
+            let quote = this.state.words[Math.floor(Math.random() * this.state.words.length)];
+
+            this.setState({newquote: quote})
+
+        },3000)
+
+        return <h1> {this.state.newquote } </h1>
+    } 
+
 
     render(){
         return(
             <div className="container">
-                <h2> {this.words} </h2>
+                <h2> {this.displayquotes() } </h2>
             </div>
         )
     }
